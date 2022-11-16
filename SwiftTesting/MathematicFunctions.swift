8b4697a6a78fce538ff5428bf0cfe632b1c89b1a
fatihfilizol <fatihfilizol@gmail.com>
2022-11-16 23:22:11 +0300

    project added

---
+++ SwiftTesting/MathematicFunctions.swift
@@ -0,0 +1,29 @@
+//
+//  MathematicFunctions.swift
+//  SwiftTesting
+//
+//  Created by Fatih Filizol on 16.11.2022.
+//
+
+import Foundation
+
+
+class MatematicFunctions {
+    
+    func addIntegers (x: Int, y: Int) -> Int {
+        return x + y
+    }
+    
+    func multiplyIntegers (x: Int, y: Int) -> Int {
+        return x * y
+    }
+
+    
+    func divideIntegers (x: Int, y: Int) -> Int {
+        return x / y
+    }
+
+    
+}
+    
+    
--- SwiftTestingUITests/SwiftTestingUITests.swift
+++ SwiftTestingUITests/SwiftTestingUITests.swift
@@ -9,33 +9,55 @@ import XCTest
 
 final class SwiftTestingUITests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-
-        // In UI tests it is usually best to stop immediately when a failure occurs.
-        continueAfterFailure = false
-
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+    func testToDoItem () throws {
+        
+        
+        let app = XCUIApplication()
+        app.launch()
+        
+        let addButton = app.navigationBars["SwiftTesting.View"].buttons["Add"]
+        let elementsQuery = app.alerts["Add Item"].scrollViews.otherElements
+        let textField = elementsQuery.collectionViews.cells.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element(boundBy: 1)
+        
+        let okButton = elementsQuery.buttons["OK"]
+        let addedCell = app.tables.cells.containing(.staticText, identifier:"my to do list").element
+        
+        addButton.tap()
+        textField.tap()
+        textField.typeText("my to do list")
+        okButton.tap()
+        
+        XCTAssertTrue(addedCell.exists)
+                
+              
+        
     }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // UI tests must launch the application that they test.
+    
+    func testDeleteItem () throws {
+        
+        
         let app = XCUIApplication()
         app.launch()
 
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
+        
+        let addButton = app.navigationBars["SwiftTesting.View"].buttons["Add"]
+        let elementsQuery = app.alerts["Add Item"].scrollViews.otherElements
+        let textField = elementsQuery.collectionViews.cells.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element(boundBy: 1)
+        
+        let okButton = elementsQuery.buttons["OK"]
+        let addedCell = app.tables.cells.containing(.staticText, identifier:"my to do list").element
+        
+        addButton.tap()
+        textField.tap()
+        textField.typeText("my to do list")
+        okButton.tap()
 
-    func testLaunchPerformance() throws {
-        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
-            // This measures how long it takes to launch your application.
-            measure(metrics: [XCTApplicationLaunchMetric()]) {
-                XCUIApplication().launch()
-            }
-        }
+        let tablesQuery = app.tables
+        addedCell.swipeLeft()
+        tablesQuery.buttons["Delete"].tap()
+        
+        
+        
     }
+    
 }
